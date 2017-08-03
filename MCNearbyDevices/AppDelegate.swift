d93822a1fc2e4f5c078a8f11305aa7580f8a6ed5
Marcin Jackowski <marcinjackowski@Marcins-MacBook-Pro.local>
2017-08-03 20:17:04 +0200

    initial commit

---
+++ MCNearbyDevices/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MCNearbyDevices
+//
+//  Created by Marcin Jackowski on 03/08/2017.
+//  Copyright © 2017 Marcin Jackowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MCNearbyDevices/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MCNearbyDevices
+//
+//  Created by Marcin Jackowski on 03/08/2017.
+//  Copyright © 2017 Marcin Jackowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
